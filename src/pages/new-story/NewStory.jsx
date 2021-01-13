000a37f2ab85ee5da09dbb24ad0178a905751543
elinoza <hillcakmak@gmail.com>
2021-01-14 00:37:23 +0300

    write story done

--- src/pages/new-story/NewStory.jsx
+++ src/pages/new-story/NewStory.jsx
@@ -3,6 +3,7 @@ import ReactQuill from "react-quill";
 import { Container } from "react-bootstrap";
 import "react-quill/dist/quill.bubble.css";
 import { Button } from "react-bootstrap";
+import Parser from 'html-react-parser';
 import "./styles.scss";
 import CategoryPicker from "../../components/CategoryPicker";
 
@@ -10,12 +11,69 @@ import CategoryPicker from "../../components/CategoryPicker";
 export default class NewStory extends Component {
   state = {
     html: "",
+    topic:'',
+    data:{
+      headLine:'',
+      subHead:'',
+      content:'',
+      category:{
+        name:'', 
+        img:''},
+      author:{ 
+        name:'', 
+        img:''},
+      cover:''
+
+    },
   };
 
   editor = React.createRef();
-  onChange = (html) => {
-    this.setState({ html })
-    console.log(html)
+
+  postFetch= async()=>{ 
+    const url=process.env.REACT_APP_URL
+    try {
+      let response= await fetch(url+ "/articles",
+      { 
+      method: "POST",
+      body: JSON.stringify(this.state.data),
+      headers: new Headers({
+      "Content-Type": "application/json"})
+      })
+      if (response.ok){
+        console.log("article posted successfully")
+       }
+      else{
+        console.log("an error occurred")
+        let error = await response.json()
+       console.log(error)}
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+  ParseChange=(html)=>{
+    let  htmlString = html
+    let  parsedHtml= htmlString.replace(/<[^>]+>/g, '')
+   
+    let data={...this.state.data}
+    let content= "content"
+    data[content] = parsedHtml
+    this.setState({data})
+    
+    console.log(parsedHtml)
+
+  }
+  onChange = (e) => {
+    
+   
+    let data={...this.state.data}
+    let currentid = e.currentTarget.id
+		data[currentid] = e.currentTarget.value 
+    this.setState({data:data})
+    
+   
+   
+
   };
   onKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -28,12 +86,21 @@ export default class NewStory extends Component {
     return (
       <Container className="new-story-container" expand="md">
         <div className="category-container">
-        <CategoryPicker onChange={(topic)=>{console.log(topic)}} />
+        <CategoryPicker onChange={(topic)=>{
+           let data={...this.state.data}
+           let category= "category"
+           data[category].name = topic.name
+           data[category].img= topic.img
+           this.setState({data})
+           }} />
         </div>
         <input
           onKeyDown={this.onKeyDown}
           placeholder="Title"
           className="article-title-input"
+          id="headLine"
+          value={this.state.data.headLine}
+          onChange={this.onChange}
         />
 
         <ReactQuill
@@ -41,17 +108,21 @@ export default class NewStory extends Component {
           formats={NewStory.formats}
           ref={this.editor}
           theme="bubble"
-          value={html}
-          onChange={this.onChange}
+          // value={this.state.data.content}
+          onChange={this.ParseChange}
           placeholder="Tell your story..."
         />
         <input
           onKeyDown={this.onKeyDown}
           placeholder="Cover link e.g : https://picsum.photos/800"
           className="article-cover-input"
+          id="cover"
+          value={this.state.data.cover}
+          onChange={this.onChange}
         />
+        {this.state.data.content}
        
-        <Button variant="success" className="post-btn">
+        <Button variant="success" className="post-btn" onClick={this.postFetch}>
           Post
         </Button>
       </Container>
